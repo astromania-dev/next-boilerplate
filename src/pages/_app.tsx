3798432657fd79e6b0fd2605b60a4401725cb561
Irving Dinh <irving.dinh@gmail.com>
2022-12-05 17:33:47 +0700

    Use the local version of `Inter.var.woff2` instead of CDN to reduce the DNS lookup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,24 @@
-import type { AppProps } from 'next/app'
+import localFont from '@next/font/local'
+import type {AppProps} from 'next/app'
 
 import '../styles/base.css'
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const inter = localFont({
+  src: '../assets/fonts/Inter.var.woff2',
+})
+
+export default function MyApp({Component, pageProps}: AppProps) {
+  return (
+    <>
+      <style global jsx>
+        {`
+          :root {
+            --font-inter: ${inter.style.fontFamily};
+          }
+        `}
+      </style>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,7 @@ import {Html, Head, Main, NextScript} from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css"/>
-      </Head>
+      <Head/>
       <body>
         <Main/>
         <NextScript/>
